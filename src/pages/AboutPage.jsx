ad8ad1c6365105a48f43e50f1fadfd2d169de0e0
joao <jstreibel@gmail.com>
2026-08-12 16:25:49 -0300

    Added some text to 'About' section

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,17 @@
 export default function AboutPage() {
   return <>
-    <h1>About</h1>
-    Software developer
-  </>
+    <h1>Greetings</h1>
+    And welcome.
+
+    <br></br><br></br>
+
+    I'm a software engineer with a PhD in Physics.
+    While I love tweaking stuff, learning new things and having little side-projects,
+    my medium-long term focus is set on expanding my experience and knowledge base
+    into Machine Learning.
+
+    <br></br><br></br>
+
+    A few years ago, while researching aspects of thermal systems,
+    </>
 }
